7077c8610fc72b58ccc90db1e39c08cdd6554bdc
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-04-18 08:13:01 +0300

    enable submit on enter press

--- src/MessageInput.js
+++ src/MessageInput.js
@@ -17,6 +17,7 @@ const MessageInput = ({ onSend }) => {
         rows="15"
         placeholder="What's up?"
         value={message}
+        onKeyPress={ev => ev.key === 'Enter' && sendAndClean()}
         onChange={ev => setMessage(ev.target.value)}
       />
       <button onClick={sendAndClean}>Send</button>
