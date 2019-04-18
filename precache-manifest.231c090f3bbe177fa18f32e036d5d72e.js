81ad94703b49ab86ac394fd27ae3459d933d028b
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-04-18 15:43:21 +0300

    Updates

---
+++ precache-manifest.231c090f3bbe177fa18f32e036d5d72e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfa66f831cf15850b5cc",
+    "url": "/conversation/static/js/runtime~main.96dda8ab.js"
+  },
+  {
+    "revision": "3331a17a23945993e9d0",
+    "url": "/conversation/static/js/main.d2df2e0f.chunk.js"
+  },
+  {
+    "revision": "221f781e43a0fc7c3512",
+    "url": "/conversation/static/js/2.ba5b4cb2.chunk.js"
+  },
+  {
+    "revision": "3331a17a23945993e9d0",
+    "url": "/conversation/static/css/main.e0b32fe1.chunk.css"
+  },
+  {
+    "revision": "c1bd779adefe41c6d4accc45992aa023",
+    "url": "/conversation/index.html"
+  }
+];
